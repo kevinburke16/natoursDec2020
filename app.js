1a1931a4df738c16e2d7a482a5ce903397396bb2
kevin <kjb16@hotmail.com>
2021-01-06 02:20:51 -0500

    removed type from express raw

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.post('/webhook-checkout', express.raw({type: 'application/json'}), bookingController.webhookCheckout)
+app.post('/webhook-checkout', express.raw(), bookingController.webhookCheckout)
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
