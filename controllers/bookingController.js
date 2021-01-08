9107ae7c45cdf84e959a3a844ef0c033c6475c64
kevin <kjb16@hotmail.com>
2021-01-08 17:56:51 -0500

    chaned booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -56,7 +56,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({email: session.customer_email})).id
-  const price = session.line_items[0].total / 100;
+  const price = session.amount_total / 100;
   console.log(tour,price,user)
   await Booking.create({ tour, user, price });
 
