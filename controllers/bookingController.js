599cc54a56e1c3bc336a4a3d36f8aeadc89c34be
kevin <kjb16@hotmail.com>
2021-01-08 13:26:14 -0500

    changed order amounconst createBookingCheckout = async session => {

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -57,6 +57,7 @@ const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({email: session.customer_email})).id
   const price = session.amount_total / 100;
+  console.log(tour,price,user)
   await Booking.create({ tour, user, price });
 
 }
